51a698471780fa3d68e126c461e14045efe9fe59
jonas collin <11700223@student.pxl.be>
2020-07-06 19:20:33 +0200

    Commit #4 authentication check works

--- Carpool/app/src/main/java/com/example/carpool/LogInFragment.kt
+++ Carpool/app/src/main/java/com/example/carpool/LogInFragment.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_login.view.*
 
 class LogInFragment : Fragment(){
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -14,4 +20,19 @@ class LogInFragment : Fragment(){
     ): View? {
         return inflater.inflate(R.layout.fragment_login,container,false)
     }
+
+
+    fun getEditTexts() {
+        val editTexts = arrayListOf<EditText>()
+        val emailInput = view?.findViewById(R.id.UsernameInput) as EditText
+        val passwordInput = view?.findViewById(R.id.PasswordInput) as EditText
+        editTexts.add(emailInput)
+        editTexts.add(passwordInput)
+    }
+
+
+
+
+
+
 }
--- Carpool/app/src/main/java/com/example/carpool/MainActivity.kt
+++ Carpool/app/src/main/java/com/example/carpool/MainActivity.kt
@@ -2,12 +2,54 @@ package com.example.carpool
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.annotation.NonNull
+import androidx.fragment.app.Fragment
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
-
+    private val authenticator = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    fun handleClick(view: View) {
+        val fragment = LogInFragment()
+
+        val email = findViewById<EditText>(R.id.UsernameInput)
+        val password = findViewById<EditText>(R.id.PasswordInput)
+
+
+            authenticator.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Toast.makeText(baseContext, "Authentication success.",
+                            Toast.LENGTH_SHORT).show()
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+
+                    }
+
+
+
+
+
+                }
 
     }
 }
