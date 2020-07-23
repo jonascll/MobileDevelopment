36162084c3a66020074365bcca6e252d7b705f79
jonas collin <11700223@student.pxl.be>
2020-07-24 01:25:46 +0200

    Commit #22 created layout + set values of request

--- Carpool/app/src/main/java/com/example/carpool/RequestedDriveDetailActivity.kt
+++ Carpool/app/src/main/java/com/example/carpool/RequestedDriveDetailActivity.kt
@@ -1,6 +1,7 @@
 package com.example.carpool
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class RequestedDriveDetailActivity : AppCompatActivity(){
@@ -14,6 +15,19 @@ class RequestedDriveDetailActivity : AppCompatActivity(){
         val destination = intent.getStringExtra("destination")
         val poolerUid = intent.getStringExtra("poolerUid")
         val requesterUid = intent.getStringExtra("requesterUid")
-        //TODO : implement layout correctly
+        val emailField : TextView = findViewById(R.id.emailRequestedDriveDetail)
+        val startCityField : TextView = findViewById(R.id.startCityRequestedDriveDetail)
+        val startAddressField : TextView = findViewById(R.id.startAddressRequestedDriveDetail)
+        val endCityField : TextView = findViewById(R.id.endCityRequestedDriveDetail)
+        val destinationField : TextView = findViewById(R.id.endAddressRequestedDriveDetail)
+        val poolerUidField : TextView = findViewById(R.id.poolerUidRequestedDriveDetail)
+        val requesterUidField : TextView = findViewById(R.id.requesterUidRequestedDriveDetail)
+        emailField.text = email
+        startCityField.text = startCity
+        startAddressField.text = startAddress
+        endCityField.text = endCity
+        destinationField.text = destination
+        poolerUidField.text = poolerUid
+        requesterUidField.text = requesterUid
     }
 }
